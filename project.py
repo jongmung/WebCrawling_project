32ea56d8e2c2980fb9a3a52be0872a6a06c94778
jongmung <jmp99@naver.com>
2022-12-13 02:53:05 +0900

    food_eatery

--- project.py
+++ project.py
@@ -12,11 +12,11 @@ bs = BeautifulSoup(r.text, "lxml")
 #print(bs)
 lis = bs.select("ul.lst_total > li.bx")
 
-df = pd.DataFrame(columns=['글자수','이미지수','제목','발행일','위치'])
+df = pd.DataFrame(columns=['식당이름','제목','위치','발행일','공감'])
 
 for cnt, i in enumerate(lis):
     print(f"블로그{cnt+1} / 전체블로그 {len(lis)}")
-    if cnt > 10:
+    if cnt > 2:
         break
     link = i.select_one("a.api_txt_lines.total_tit").get("href")
     thumb = i.select_one("a.sub_thumb").get("href")
@@ -36,16 +36,19 @@ for cnt, i in enumerate(lis):
     if (bs.select_one("div.pcol1 > div > p > span") is None):
         continue
     else:
+        eatery = bs.select_one("div.se-module-map-text > a > strong.se-map-title").text
         title = bs.select_one("div.pcol1 > div > p > span").text
         pub_date = bs.select_one("div.blog2_container > span.se_publishDate").text
-        contents = bs.select_one("div.se-main-container").text.replace("\n", "")
-        images = bs.select_one("div.se-main-container").select("img")
-        locate = bs.select_one("div.se-module-map-text").text
-        total_len = len(contents)
-        img_len = len(images)
-        print(f"전체 글자수: {total_len} 이미지갯수: {img_len} 제목: {title} 발행일 : {pub_date} 위치 : {locate}")
+        #contents = bs.select_one("div.se-main-container").text.replace("\n", "")
+        #images = bs.select_one("div.se-main-container").select("img")
+        locate = bs.select_one("div.se-module-map-text > a > p.se-map-address").text
+        heart = bs.select_one("div.u_likeit_list_module > span.u_likeit_list_btn > em.u_txt").text
+        #heart_len = len(heart)
+        #total_len = len(contents)
+        #img_len = len(images)
+        print(f"식당이름: {eatery} 제목: {title} 위치 : {locate} 발행일 : {pub_date} 공감 : {heart}")
         #print(f"데이터: {container}")
-        df = df.append(pd.DataFrame([[total_len, img_len, title, pub_date, locate]], columns=['글자수', '이미지수', '제목', '발행일', '위치']), ignore_index=True)
+        df = df.append(pd.DataFrame([[eatery, title, locate, pub_date, heart]], columns=['식당이름','제목', '위치', '발행일', '공감']), ignore_index=True)
         
 print(df)
 df.to_csv('./Food_blog.csv', encoding='utf-8')
