6cd8f8fed05221e160f99153e0d4d7e2e9236ecf
jongmung <jmp99@naver.com>
2022-12-15 03:06:40 +0900

    final_project

--- project.py
+++ project.py
@@ -1,3 +1,4 @@
+from contextlib import nullcontext
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -12,11 +13,11 @@ bs = BeautifulSoup(r.text, "lxml")
 #print(bs)
 lis = bs.select("ul.lst_total > li.bx")
 
-df = pd.DataFrame(columns=['식당이름','제목','위치','발행일','공감'])
+df = pd.DataFrame(columns=['식당이름','제목','위치','발행일','댓글수'])
 
 for cnt, i in enumerate(lis):
     print(f"블로그{cnt+1} / 전체블로그 {len(lis)}")
-    if cnt > 2:
+    if cnt > 5:
         break
     link = i.select_one("a.api_txt_lines.total_tit").get("href")
     thumb = i.select_one("a.sub_thumb").get("href")
@@ -36,19 +37,19 @@ for cnt, i in enumerate(lis):
     if (bs.select_one("div.pcol1 > div > p > span") is None):
         continue
     else:
-        eatery = bs.select_one("div.se-module-map-text > a > strong.se-map-title").text
-        title = bs.select_one("div.pcol1 > div > p > span").text
-        pub_date = bs.select_one("div.blog2_container > span.se_publishDate").text
-        #contents = bs.select_one("div.se-main-container").text.replace("\n", "")
-        #images = bs.select_one("div.se-main-container").select("img")
-        locate = bs.select_one("div.se-module-map-text > a > p.se-map-address").text
-        heart = bs.select_one("div.u_likeit_list_module > span.u_likeit_list_btn > em.u_txt").text
-        #heart_len = len(heart)
-        #total_len = len(contents)
-        #img_len = len(images)
-        print(f"식당이름: {eatery} 제목: {title} 위치 : {locate} 발행일 : {pub_date} 공감 : {heart}")
-        #print(f"데이터: {container}")
-        df = df.append(pd.DataFrame([[eatery, title, locate, pub_date, heart]], columns=['식당이름','제목', '위치', '발행일', '공감']), ignore_index=True)
+        eatery = bs.select_one("div.se-module-map-text > a > strong.se-map-title").text #식당이름
+        title = bs.select_one("div.pcol1 > div > p > span").text                        #블로그제목
+        pub_date = bs.select_one("div.blog2_container > span.se_publishDate").text      #발행일
+        locate = bs.select_one("div.se-module-map-text > a > p.se-map-address").text    #위치
+        if (bs.select_one("a.btn_comment > em._commentCount") is None):                 #댓글
+            post = "x"
+        else:
+            post = bs.select_one("a.btn_comment > em._commentCount").text
+        #만약 블로그 댓글기능이 제한이라면 x
+        #달려있는 댓글이 없는 상태라면 빈칸
+
+        print(f"식당이름: {eatery} 제목: {title} 위치 : {locate} 발행일 : {pub_date}  댓글수 : {post}")
+        df = df.append(pd.DataFrame([[eatery, title, locate, pub_date,  post]], columns=['식당이름','제목', '위치', '발행일','댓글수']), ignore_index=True)
         
 print(df)
-df.to_csv('./Food_blog.csv', encoding='utf-8')
+df.to_csv('./맛집리스트.csv', encoding='utf-8')
